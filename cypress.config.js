11bd87b67d1f7d0700c75dce8f77bc3aebdf93eb
Lucas Camargo <lucas.nascimento@sabium.com.br>
2025-01-02 15:58:52 -0300

    Removendo arquivos não necessários. / 01.01.2024

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
 const { defineConfig } = require("cypress");
 
+// module.exports = defineConfig({
+//   e2e: {
+//     "baseUrl": "http://localhost:8091/sabium#/",
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     }
+//   },
+// });
+
 module.exports = defineConfig({
   e2e: {
-    "baseUrl": "http://localhost:8091/sabium#/",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    }
+    env: {
+      API_URL: 'http://localhost:8091/sabium#/'
+    } 
   },
-});
+  fixturesFolder: false,
+})
--- cypress/e2e/filial/get_filial_detalhe.cy.js
+++ cypress/e2e/filial/get_filial_detalhe.cy.js
@@ -1,32 +1,43 @@
-// /v3/filial_detalhe/{filial} - Dados da Filial
-// Retorna dados cadastrais da filial.
+const API_URL = Cypress.env('API_URL')
+const Authorization = Cypress.env('API.PRAGMA')
+const filial = 10050  // ID da filial a ser consultada
 
-import acess_token from '../../fixtures/token.json'
-import pragma_token from '../../fixtures/pragma.json'
+describe('', { env: { hideCredendials: true } }, () => {
 
-describe('Filial - GET - /v3/filial_detalhe/{filial}', () => {
-  const url = '/Filial/v2_filial_detalhe';
-  const token = acess_token
-  
-    it('Resposta 200', () => {
-      const requestBody = {
-        filial: ""
-      }
+  it('teste API', () => {
+    cy.api({
+      method: 'GET',
+      url: `${API_URL}/Filial/v2_filial_detalhe/${filial}`,
+      headers: { Authorization }
+    }).should(({ status, body }) => {
+      const { data } = body
+      expect(status).to.equal(200)
+      expect(resposta.body.retorno[0]).toHaveProperty('id_filial');
+      expect(resposta.body.retorno[0]).toHaveProperty('id_empresa');
+      expect(resposta.body.retorno[0]).toHaveProperty('numero_filial');
+      expect(resposta.body.retorno[0]).toHaveProperty('nome_fantasia');
+      expect(resposta.body.retorno[0]).toHaveProperty('razao_social');
+      expect(resposta.body.retorno[0]).toHaveProperty('cnpj');
+      expect(resposta.body.retorno[0]).toHaveProperty('cce');
+      expect(resposta.body.retorno[0]).toHaveProperty('tipo_contribuinte');
+      expect(resposta.body.retorno[0]).toHaveProperty('inscricao_municipal');
+      expect(resposta.body.retorno[0]).toHaveProperty('inscricao_suframa');
+      expect(resposta.body.retorno[0]).toHaveProperty('endereco');
+      expect(resposta.body.retorno[0]).toHaveProperty('numero');
+      expect(resposta.body.retorno[0]).toHaveProperty('bairro');
+      expect(resposta.body.retorno[0]).toHaveProperty('complemento');
+      expect(resposta.body.retorno[0]).toHaveProperty('id_cidade');
+      expect(resposta.body.retorno[0]).toHaveProperty('cidade');
+      expect(resposta.body.retorno[0]).toHaveProperty('uf');
+      expect(resposta.body.retorno[0]).toHaveProperty('cep');
+      expect(resposta.body.retorno[0]).toHaveProperty('caixa_postal');
+      expect(resposta.body.retorno[0]).toHaveProperty('id_gruporota');
+      expect(resposta.body.retorno[0]).toHaveProperty('id_rotacidade');
+      expect(resposta.body.retorno[0]).toHaveProperty('id_ibge');
+      expect(resposta.body.retorno[0].inscsubstitutotributario[0]).toHaveProperty('uf');
+      expect(resposta.body.retorno[0].inscsubstitutotributario[0]).toHaveProperty('inscricao_substitutotributario');
+      expect(resposta.body.retorno[0].inscsubstitutotributario[0]).toHaveProperty('exclusivodifal');
 
-      cy.request({
-        method: 'GET', 
-        url: url, 
-        headers: { 
-          Pragma: pragma_token,
-          Authorization: `Bearer ${token}`,
-          //'Content-Type': 'application/json'
-        },
-        requestBody,
-        failOnStatusCode: false
-      })
-        .then((response) => {
-          expect(response.status).to.eq(200);
-          expect(response.duration).to.be.below(2000); 
-        });
-    });
-  });
+    })
+  })
+})
