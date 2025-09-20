e8c4bf89492f920e0f35330db8ecb0909d360dfd
Lucas Camargo <nascimentocamargolucas@gmail.com>
2025-09-20 14:09:33 -0300

    Refactor Cypress configuration to use environment variable for API_URL and add .env file

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,11 @@
 const { defineConfig } = require("cypress");
-
-// module.exports = defineConfig({
-//   e2e: {
-//     "baseUrl": "http://localhost:8091/sabium#/",
-//     setupNodeEvents(on, config) {
-//       // implement node event listeners here
-//     }
-//   },
-// });
+require('dotenv').config();
 
 module.exports = defineConfig({
   e2e: {
     env: {
-      API_URL: 'http://localhost:8091/sabium#/'
-    } 
+      API_URL: process.env.API_URL
+    }
   },
   fixturesFolder: false,
 })
